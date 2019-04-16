243e7f2ec4b9dd9d9eca1fbf7be4acfa40a6d88e
Ubuntu <ubuntu@ip-172-31-26-82.us-east-2.compute.internal>
2019-04-16 19:05:51 +0000

    Inicio da criação do Login para o BackEnd do sistema

---
+++ app/rails/app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+  
+end
--- app/rails/config/routes.rb
+++ app/rails/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'home' => 'welcome/index'
+  root 'welcome#index'
+  
+  get 'sign_in' => 'sessions#new'
+  post 'sign_in' => 'sessions#create'
+  delete 'sign_out' => 'sessions#destroy'
+  
+  
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
