bf5cc940ef2e17730730ff4239c82070716abaf8
Danilo Lira <drla@cin.ufpe.br>
2019-04-26 20:44:48 +0000

    Teste

--- app/rails/db/schema.rb
+++ app/rails/db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 2019_04_17_130751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
