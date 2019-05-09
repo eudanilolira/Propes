7ff7cfaf99a51c45804752fabea6faefa1f2ff48
Ubuntu <ubuntu@ip-172-31-41-19.us-east-2.compute.internal>
2019-05-09 01:06:04 +0000

    Adição inicial das features e step definitions

--- propes-app/db/schema.rb
+++ propes-app/db/schema.rb
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_031430) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
---
+++ propes-app/features/step_definitions/User_access.rb
@@ -0,0 +1,35 @@
+Given(/^I'm in login module$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^click on the button "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the app shall redirect me to Meu Perfil module$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I'm in the login module$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill the field name$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on the button "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the system shall show the message: "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^i fill the field password$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill the field "([^"]*)" com "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
