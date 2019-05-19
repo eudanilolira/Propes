97dd47512da37e606cafbc4e3b872f8bcf5c45b7
Danilo Lira <drla@cin.ufpe.br>
2019-05-19 04:59:04 +0000

    Embelezamento do sistema

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Propes
     config.load_defaults 5.2
     I18n.available_locales = [:en, :pt]
     I18n.default_locale = :pt
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
