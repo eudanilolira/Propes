87770306807cd321c2a8398066343d3df381c598
Danilo Lira <drla@cin.ufpe.br>
2019-08-08 09:21:42 -0300

    Mudanças de segurança no database

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,17 @@ module Propes
     
     config.autoload_paths += %W(#{config.root}/lib) # Line added by developers.
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
+
